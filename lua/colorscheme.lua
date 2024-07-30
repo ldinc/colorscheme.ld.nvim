a2c3b695ec2e84ab4dc285f9b6d25476793943a9
ldinc <drogunov.igor@gmail.com>
2024-07-31 02:22:43 +0300

    dev: refine

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,8 +1,22 @@
 local colorscheme = {}
+local pallete = require('lua.base.pallete')
 
 function colorscheme.load()
-    vim.g.colors_name = "ld"
-    vim.o.termguicolors = true
+    local o = vim.o
+    local g = vim.g
+    local set = vim.api.nvim_set_hl
+
+    g.colors_name = "ld"
+    o.background = "light"
+    o.termguicolors = true
+
+    local config = {
+        {0, "normal", { fg = pallete.base.dark, bg = pallete.base.light}}
+    }
+
+    for _, param in config do
+        set(param)
+    end
 
     -- TODO: add highlight processor here
 
